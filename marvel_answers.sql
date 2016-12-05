213a6302e4a30c9cb4c6bbd9ec46c5a795679b2b
andrew guppy <andrew.guppy@blueyonder.co.uk>
2016-12-05 23:32:17 +0000

    added deleting of more than 1

--- marvel_answers.sql
+++ marvel_answers.sql
@@ -25,3 +25,27 @@ INSERT INTO movies (title, year, show_time) VALUES ('Guardians of the Galaxy 2',
 
 -- in answer to question 9
 UPDATE movies SET show_time = '21:30' WHERE title = 'Guardians of the Galaxy';
+
+-- enhanced homework
+
+--delete  from movies table id's 7,8,9,10 and 11
+DELETE FROM movies WHERE id BETWEEN 7 AND 11;
+SELECT * FROM movies;
+
+--Delete all duplicates 'Simon Dougas' from people 
+-- insert some Simon Douglas dodgey characters
+INSERT INTO people (name) VALUES ('Simon Douglas'),('Simon Douglas'),('Simon Douglas'),('Simon Douglas'),('Simon Douglas'),('Simon Douglas');
+-- demonstrate they exist
+SELECT * FROM people;
+-- delete duplicates
+DELETE FROM people WHERE id NOT IN (SELECT min(id) FROM people GROUP BY name);
+-- show  they are gone
+SELECT * FROM people;
+
+--After running the answers script several times Guardians of the Galaxy 2 has been inserted multiple times, this will Delete all duplicates 'Guardians of the Galaxy 2' from movies
+-- demonstrate they exist
+SELECT * FROM movies;
+-- delete duplicates
+DELETE FROM movies WHERE id NOT IN (SELECT min(id) FROM movies GROUP BY title);
+-- show  they are gone
+SELECT * FROM movies;
